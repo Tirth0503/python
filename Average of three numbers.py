e74ff46bfa1e27fe135305b9dcc7472f0c49a407
TIRTH DAVE <tirthdave3011@gmail.com>
2026-08-01 14:36:15 +0530

    Add files via upload

---
+++ Average of three numbers.py
@@ -0,0 +1,5 @@
+# Average of three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print("Average =", (a + b + c) / 3)
